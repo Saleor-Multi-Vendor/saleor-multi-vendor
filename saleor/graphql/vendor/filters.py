b0e002b4b8f15cfd409f31abde70de8aab5ce9c8
calcgen2 <raj.shrestha778@gmail.com>
2021-08-17 16:46:41 +0545

    Add query for vendor by id. Add filter to query vendor.

---
+++ saleor/graphql/vendor/filters.py
@@ -0,0 +1,37 @@
+import django_filters
+from graphene_django.filter import GlobalIDMultipleChoiceFilter
+
+from ...vendor.models import Vendor
+from ..core.types import FilterInputObjectType
+from ..utils.filters import filter_by_query_param
+
+
+def prefetch_qs_for_filter(qs):
+    return qs.prefetch_related("user")
+
+
+def filter_search_vendor(qs, _, value):
+    search_fields = {
+        "shop_name",
+        "user__email",
+        "user__first_name",
+    }
+    if value:
+        qs = prefetch_qs_for_filter(qs)
+        qs = filter_by_query_param(qs, value, search_fields)
+
+    return qs
+
+
+class VendorFilter(django_filters.FilterSet):
+    search = django_filters.CharFilter(method=filter_search_vendor)
+    id = GlobalIDMultipleChoiceFilter(field_name="id")
+
+    class Meta:
+        model = Vendor
+        fields = []
+
+
+class VendorFilterInput(FilterInputObjectType):
+    class Meta:
+        filterset_class = VendorFilter
--- saleor/graphql/vendor/schema.py
+++ saleor/graphql/vendor/schema.py
@@ -1,21 +1,38 @@
 import graphene
+
+from .filters import VendorFilterInput
 from .mutations import (
     VendorCreate,
     VendorDelete,
     VendorUpdate,
     VendorWarehouseCreate,
     VendorWarehouseDelete,
-    VendorWarehouseUpdate
+    VendorWarehouseUpdate,
 )
+from .types import Vendor
 
-# registering the mutaion for schema here which takes automatically when 
+
+# registering the mutaion for schema here which takes automatically when
 # run command npm run build-schema
 class VendorMutations(graphene.ObjectType):
     create_vendor = VendorCreate.Field()
     delete_vendor = VendorDelete.Field()
     update_vendor = VendorUpdate.Field()
 
+
 class VendorWarehouseMutation(graphene.ObjectType):
     create_vendorWarehouse = VendorWarehouseCreate.Field()
     update_vendorWarehouse = VendorWarehouseUpdate.Field()
     delete_vendorWarehouse = VendorWarehouseDelete.Field()
+
+
+class VendorQueries(graphene.ObjectType):
+    vendor = graphene.Field(
+        Vendor,
+        description="Look up a vendor by ID.",
+        id=graphene.Argument(graphene.ID, description="ID of a vendor", required=True),
+    )
+
+    vendors = graphene.Field(
+        Vendor, description="List of vendors.", filter=VendorFilterInput()
+    )
